889256b35cf45a1d1207fd850cc5c93cb7f576b1
Noela-Murugi <murugin254@gmail.com>
2022-08-28 20:27:13 +0300

    create a function getImage details -fetch details

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 // write your code here
 document.addEventListener('DOMContentLoaded', () => {
+function getImageDetails(id){
+    fetch (`http://localhost:3000/images/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+        document.querySelector('#card-title').textContent = data.title;
+        document.querySelector('#card-image').src = data.image;
+        document.querySelector('#like-button').addEventListener('click', function (){
+            let countLike = document.querySelector('#like-count')
+            data.likes ++;
+            countLike.textContent = `${data.likes} likes`;
+        })
+    })
+}
+getImageDetails(1)
 
 
 })
